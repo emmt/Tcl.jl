91270cd38e24ba9a926bbc68525d78e547409370
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-05-04 21:45:22 +0200

    Add dialog tests

---
+++ test/testdialog.jl
@@ -0,0 +1,19 @@
+module TclDialogTests
+
+using Tcl
+
+function runtests()
+    interp = Tcl.TclInterp()
+    answer = Tcl.messagebox(interp; message="Really quit?", icon="question",
+        buttons="yesno", detail="Select \"Yes\" to make the application exit")
+    if answer == "yes"
+        #quit()
+        Tcl.messagebox(interp; message="Too bad, bye bye...", buttons="ok")
+
+    elseif answer == "no"
+        Tcl.messagebox(interp; message="I know you like this application!",
+            buttons="ok")
+    end
+end
+
+end # module
